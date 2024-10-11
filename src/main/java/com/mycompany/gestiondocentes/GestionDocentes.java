d1668b30900b52e9f9475cd51f901a5246939f5b
gonza <>
2024-10-11 17:05:32 -0300

    no comment, said coco

---
+++ src/main/java/com/mycompany/gestiondocentes/GestionDocentes.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestiondocentes;
+
+/**
+ *
+ * @author gonza
+ */
+public class GestionDocentes {
+
+    public static void main(String[] args) {
+        Docente docente = new Docente();
+        System.out.println("");
+    }
+}
