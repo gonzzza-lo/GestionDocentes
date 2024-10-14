fe88b7736066eddf756c00af50963e7197c3df4e
gonza <>
2024-10-14 15:26:28 -0300

    actualizacion

--- src/main/java/com/mycompany/gestiondocentes/GestionDocentes.java
+++ src/main/java/com/mycompany/gestiondocentes/GestionDocentes.java
@@ -17,6 +17,17 @@ public static Scanner in = new Scanner(System.in);
         Docente docente = new Docente();
         System.out.println("Ingrese numero de legajo: ");
         docente.numeroDeLegajo = in.nextInt();
+        System.out.println("Ingrese nombre y apellido: ");
+        docente.nombreYapellido = in.next();
+        System.out.println("Ingrese fecha de nacimiento: ");
+        docente.fechaDeNacimiento = in.nextInt();
+        System.out.println("Ingrese la edad: ");
+        docente.edad = in.nextInt();
+        System.out.println("Ingrese el salario: ");
+        docente.salario = in.nextFloat();
+        System.out.println("Ingrese si es titular: ");
+        docente.titular = in.nextBoolean();
+        
         
     }
 }
